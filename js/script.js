b7dddad44e8dbf299bb852f476324506837d0836
Darci Rodrigues <73960815+DarciRodrigues@users.noreply.github.com>
2024-09-17 23:28:02 -0300

    seg.atualização

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+const imgMoon = document.querySelector("#moon");
+const imgSun = document.querySelector("#sun");
 const mudar = document.querySelector(".switch");
 const tema = document.querySelector(".slider");
 const check = document.querySelector("#check");
 
 function mudarTema() {
   if (check.checked) {
-    mudar.
-    tema.classList.add("black");
+    imgMoon.setAttribute("src", "images/Moon_fill.svg");
+    
   } else {
-    tema.classList.remove("black");
+    
   }
 }
 
